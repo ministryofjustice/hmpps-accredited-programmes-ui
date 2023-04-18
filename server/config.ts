f68fcf77e32789d4087cd7171ff2c2123878db46
George Eaton <george@dxw.com>
2023-04-18 17:06:17 +0100

    Add API config for future client
    Need to add env vars for this

--- server/config.ts
+++ server/config.ts
@@ -57,6 +57,14 @@ export default {
       systemClientId: get('SYSTEM_CLIENT_ID', 'clientid', requiredInProduction),
       systemClientSecret: get('SYSTEM_CLIENT_SECRET', 'clientsecret', requiredInProduction),
     },
+    accreditedProgrammesApi: {
+      url: get('ACCREDITED_PROGRAMMES_API_URL', 'http://localhost:9092', requiredInProduction),
+      timeout: {
+        response: Number(get('ACCREDITED_PROGRAMMES_API_TIMEOUT_RESPONSE', 10000)),
+        deadline: Number(get('ACCREDITED_PROGRAMMES_API_TIMEOUT_DEADLINE', 10000)),
+      },
+      agent: new AgentConfig(Number(get('HMPPS_AUTH_TIMEOUT_RESPONSE', 10000))),
+    },
     tokenVerification: {
       url: get('TOKEN_VERIFICATION_API_URL', 'http://localhost:8100', requiredInProduction),
       timeout: {
