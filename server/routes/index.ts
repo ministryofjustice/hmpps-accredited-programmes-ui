6c7550aac6985251057adbc4a5b0161700f3fa68
Ynda Jas <yndajas@gmail.com>
2023-05-18 12:47:40 +0100

    Remove redundant ESLint disabling line
    All the variables are used, so it's unclear why we're disabling the
    unused variables rule

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -3,7 +3,6 @@ import { type RequestHandler, Router } from 'express'
 import { Controllers } from '../controllers'
 import asyncMiddleware from '../middleware/asyncMiddleware'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function routes(controllers: Controllers): Router {
   const router = Router()
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
