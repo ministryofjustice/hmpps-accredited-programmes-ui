e1fbc16abf489cddc431b022de52f1311e504106
Ynda Jas <yndajas@gmail.com>
2023-05-23 15:15:09 +0100

    Add organisation-related models/types
    Co-authored-by: George <george@dxw.com>

---
+++ server/@types/models/Organisation.ts
@@ -0,0 +1,7 @@
+import type { OrganisationAddress } from './OrganisationAddress'
+
+export type Organisation = {
+  name: string
+  category: string
+  address: OrganisationAddress
+}
--- server/@types/models/index.d.ts
+++ server/@types/models/index.d.ts
@@ -1,2 +1,4 @@
 export type { Course } from './Course'
 export type { CoursePrerequisite } from './CoursePrerequisite'
+export type { Organisation } from './Organisation'
+export type { OrganisationAddress } from './OrganisationAddress'
