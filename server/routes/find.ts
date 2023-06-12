22cf156a3c772fdcb197e05bfaf538597760d4cb
Ynda Jas <yndajas@gmail.com>
2023-06-12 13:06:16 +0100

    Use path patterns in routes

--- server/routes/find.ts
+++ server/routes/find.ts
@@ -1,15 +1,16 @@
 import type { Router } from 'express'
 
 import type { Controllers } from '../controllers'
+import findPaths from '../paths/find'
 import actions from '../utils/routeUtils'
 
 export default function routes(controllers: Controllers, router: Router): Router {
   const { get } = actions(router)
   const { coursesController, courseOfferingsController } = controllers
 
-  get('/programmes', coursesController.index())
-  get('/programmes/:id', coursesController.show())
-  get('/programmes/:id/offerings/:courseOfferingId', courseOfferingsController.show())
+  get(findPaths.courses.index.pattern, coursesController.index())
+  get(findPaths.courses.show.pattern, coursesController.show())
+  get(findPaths.courses.offerings.show.pattern, courseOfferingsController.show())
 
   return router
 }
