0b695d02dfbee1f939d3c2e8a9da57b0a34d4680
Ynda Jas <yndajas@gmail.com>
2023-06-16 11:42:20 +0100

    Use `.govuk-link` selector instead of `a`
    Per class-based selector default
    
    The only remaining non-class-based selectors are:
    - `p`, since they are generally unclassed, inheriting from `main`'s
      `govuk-body` class
    - `div[role=listitem]` - this could probably be
      `govuk-grid-column-two-thirds`, but that's not very descriptive of
      what's being selected. Though I guess the call
      `.each((courseElement, courseElementIndex)` might make it clear enough

--- integration_tests/pages/find/courses.ts
+++ integration_tests/pages/find/courses.ts
@@ -13,7 +13,7 @@ export default class CoursesPage extends Page {
       cy.wrap(courseElement).within(() => {
         const course = presentCourse(courses[courseElementIndex])
 
-        cy.get('a').should('have.attr', 'href', findPaths.courses.show({ id: course.id }))
+        cy.get('.govuk-link').should('have.attr', 'href', findPaths.courses.show({ id: course.id }))
         cy.get('.govuk-heading-m .govuk-link').should('have.text', course.name)
 
         cy.get('p:first-of-type').then(tagContainerElement => {
