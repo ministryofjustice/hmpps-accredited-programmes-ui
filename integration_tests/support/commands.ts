07a63f7419a222ae2e2461847f819070af20b3b0
Ynda Jas <yndajas@gmail.com>
2023-07-24 17:23:29 +0100

    Add `checkAccessibility` Cypress command
    This wraps around some commands provided by cypress-axe and can be
    called to inject axe and check accessibility. Further configuration will
    be added in later commits

--- integration_tests/support/commands.ts
+++ integration_tests/support/commands.ts
@@ -2,3 +2,8 @@ Cypress.Commands.add('signIn', (options = { failOnStatusCode: true }) => {
   cy.request('/')
   return cy.task<string>('getSignInUrl').then((url: string) => cy.visit(url, options))
 })
+
+Cypress.Commands.add('checkAccessibility', () => {
+  cy.injectAxe()
+  cy.checkA11y()
+})
