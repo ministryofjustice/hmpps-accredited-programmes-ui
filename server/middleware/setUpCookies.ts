383286a0891e620b00b9a0bf8a1fd66f6ef4d79a
George Eaton <george@dxw.com>
2023-07-31 16:16:22 +0100

    Hide cookie banner if cookies have been set

--- server/middleware/setUpCookies.ts
+++ server/middleware/setUpCookies.ts
@@ -1,7 +1,15 @@
+import cookieParser from 'cookie-parser'
 import { Router } from 'express'
 
 export default function setUpCookies(): Router {
   const router = Router()
+
+  router.use(cookieParser())
+
+  router.use((req, res, next) => {
+    res.locals.acceptAnalyticsCookies = req.cookies?.acceptAnalyticsCookies
+    next()
+  })
 
   router.post('/cookie-preferences', (req, res) => {
     if (req.body.acceptAnalyticsCookies) {
