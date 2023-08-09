55e2854d25b3d72abdea1072dccc1aa0b98eef7c
Ynda Jas <yndajas@gmail.com>
2023-08-09 15:30:56 +0100

    Sort `should` methods in `Page`
    We're getting to have quite a few of these, so I'm sorting them with
    some basic logic: component name; should, shouldNot

--- integration_tests/pages/page.ts
+++ integration_tests/pages/page.ts
@@ -52,6 +52,35 @@ export default abstract class Page {
 
   manageDetails = (): PageElement => cy.get('[data-qa=manageDetails]')
 
+  shouldContainBackLink(href: string): void {
+    cy.get('.govuk-back-link').should('have.attr', 'href', href)
+  }
+
+  shouldContainNavigation(currentPath: string): void {
+    const navigationItems = [{ text: 'List of programmes', href: '/programmes' }]
+
+    cy.get('.moj-primary-navigation__item').each((navigationItemElement, navigationItemElementIndex) => {
+      const { text, href } = navigationItems[navigationItemElementIndex]
+
+      const { actual, expected } = helpers.parseHtml(navigationItemElement, text)
+      expect(actual).to.equal(expected)
+
+      cy.wrap(navigationItemElement).within(() => {
+        cy.get('a').should('have.attr', 'href', href)
+
+        if (currentPath === href) {
+          cy.get('a').should('have.attr', 'aria-current', 'page')
+        } else {
+          cy.get('a').should('not.have.attr', 'aria-current', 'page')
+        }
+      })
+    })
+  }
+
+  shouldNotContainNavigation(): void {
+    cy.get('.moj-primary-navigation').should('not.exist')
+  }
+
   shouldContainSummaryListRows(
     rows: Array<GovukFrontendSummaryListRowWithValue>,
     summaryListElement: JQuery<HTMLElement>,
@@ -86,34 +115,5 @@ export default abstract class Page {
         cy.wrap(tagElement).should('have.class', tag.classes)
       })
     })
-  }
-
-  shouldContainBackLink(href: string): void {
-    cy.get('.govuk-back-link').should('have.attr', 'href', href)
-  }
-
-  shouldContainNavigation(currentPath: string): void {
-    const navigationItems = [{ text: 'List of programmes', href: '/programmes' }]
-
-    cy.get('.moj-primary-navigation__item').each((navigationItemElement, navigationItemElementIndex) => {
-      const { text, href } = navigationItems[navigationItemElementIndex]
-
-      const { actual, expected } = helpers.parseHtml(navigationItemElement, text)
-      expect(actual).to.equal(expected)
-
-      cy.wrap(navigationItemElement).within(() => {
-        cy.get('a').should('have.attr', 'href', href)
-
-        if (currentPath === href) {
-          cy.get('a').should('have.attr', 'aria-current', 'page')
-        } else {
-          cy.get('a').should('not.have.attr', 'aria-current', 'page')
-        }
-      })
-    })
-  }
-
-  shouldNotContainNavigation(): void {
-    cy.get('.moj-primary-navigation').should('not.exist')
   }
 }
