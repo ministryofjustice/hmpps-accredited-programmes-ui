7bfb50a6e9fa0582da2fa9bb88999730e3b07dfa
Ynda Jas <yndajas@gmail.com>
2023-08-11 17:45:20 +0100

    Test 404 controller responses
    ... when organisation is not found

--- server/controllers/find/courseOfferingsController.test.ts
+++ server/controllers/find/courseOfferingsController.test.ts
@@ -1,6 +1,7 @@
 import type { DeepMocked } from '@golevelup/ts-jest'
 import { createMock } from '@golevelup/ts-jest'
 import type { NextFunction, Request, Response } from 'express'
+import createError from 'http-errors'
 
 import CourseOfferingsController from './courseOfferingsController'
 import type { CourseService, OrganisationService } from '../../services'
@@ -9,21 +10,21 @@ import { courseUtils, organisationUtils } from '../../utils'
 
 describe('CoursesOfferingsController', () => {
   describe('show', () => {
+    const token = 'SOME_TOKEN'
+    const request: DeepMocked<Request> = createMock<Request>({ user: { token } })
+    const response: DeepMocked<Response> = createMock<Response>({})
+    const next: DeepMocked<NextFunction> = createMock<NextFunction>({})
+
+    const courseService = createMock<CourseService>({})
+    const organisationService = createMock<OrganisationService>({})
+
+    const course = courseFactory.build()
+    courseService.getCourse.mockResolvedValue(course)
+
+    const courseOffering = courseOfferingFactory.build()
+    courseService.getOffering.mockResolvedValue(courseOffering)
+
     it('renders the course offering show template', async () => {
-      const token = 'SOME_TOKEN'
-      const request: DeepMocked<Request> = createMock<Request>({ user: { token } })
-      const response: DeepMocked<Response> = createMock<Response>({})
-      const next: DeepMocked<NextFunction> = createMock<NextFunction>({})
-
-      const courseService = createMock<CourseService>({})
-      const organisationService = createMock<OrganisationService>({})
-
-      const course = courseFactory.build()
-      courseService.getCourse.mockResolvedValue(course)
-
-      const courseOffering = courseOfferingFactory.build()
-      courseService.getOffering.mockResolvedValue(courseOffering)
-
       const organisation = organisationFactory.build({ id: courseOffering.organisationId })
       organisationService.getOrganisation.mockResolvedValue(organisation)
 
@@ -43,6 +44,18 @@ describe('CoursesOfferingsController', () => {
           courseOffering,
           course.name,
         ),
+      })
+    })
+
+    describe('when the organisation service returns `null`', () => {
+      it('responds with a 404', async () => {
+        organisationService.getOrganisation.mockResolvedValue(null)
+
+        const courseOfferingsController = new CourseOfferingsController(courseService, organisationService)
+        const requestHandler = courseOfferingsController.show()
+        const expectedError = createError(404)
+
+        expect(() => requestHandler(request, response, next)).rejects.toThrowError(expectedError)
       })
     })
   })
--- server/controllers/refer/referralsController.test.ts
+++ server/controllers/refer/referralsController.test.ts
@@ -1,6 +1,7 @@
 import type { DeepMocked } from '@golevelup/ts-jest'
 import { createMock } from '@golevelup/ts-jest'
 import type { NextFunction, Request, Response } from 'express'
+import createError from 'http-errors'
 
 import ReferralsController from './referralsController'
 import type { CourseService, OrganisationService } from '../../services'
@@ -9,21 +10,21 @@ import { courseUtils } from '../../utils'
 
 describe('ReferralsController', () => {
   describe('start', () => {
+    const token = 'SOME_TOKEN'
+    const request: DeepMocked<Request> = createMock<Request>({ user: { token } })
+    const response: DeepMocked<Response> = createMock<Response>({})
+    const next: DeepMocked<NextFunction> = createMock<NextFunction>({})
+
+    const courseService = createMock<CourseService>({})
+    const organisationService = createMock<OrganisationService>({})
+
+    const course = courseFactory.build()
+    courseService.getCourse.mockResolvedValue(course)
+
+    const courseOffering = courseOfferingFactory.build()
+    courseService.getOffering.mockResolvedValue(courseOffering)
+
     it('renders the referral start template', async () => {
-      const token = 'SOME_TOKEN'
-      const request: DeepMocked<Request> = createMock<Request>({ user: { token } })
-      const response: DeepMocked<Response> = createMock<Response>({})
-      const next: DeepMocked<NextFunction> = createMock<NextFunction>({})
-
-      const courseService = createMock<CourseService>({})
-      const organisationService = createMock<OrganisationService>({})
-
-      const course = courseFactory.build()
-      courseService.getCourse.mockResolvedValue(course)
-
-      const courseOffering = courseOfferingFactory.build()
-      courseService.getOffering.mockResolvedValue(courseOffering)
-
       const organisation = organisationFactory.build({ id: courseOffering.organisationId })
       organisationService.getOrganisation.mockResolvedValue(organisation)
 
@@ -39,6 +40,18 @@ describe('ReferralsController', () => {
         course: coursePresenter,
         courseOffering,
         organisation,
+      })
+    })
+
+    describe('when the organisation service returns `null`', () => {
+      it('responds with a 404', async () => {
+        organisationService.getOrganisation.mockResolvedValue(null)
+
+        const referralsController = new ReferralsController(courseService, organisationService)
+        const requestHandler = referralsController.start()
+        const expectedError = createError(404)
+
+        expect(() => requestHandler(request, response, next)).rejects.toThrowError(expectedError)
       })
     })
   })
