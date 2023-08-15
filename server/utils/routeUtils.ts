5b213e5e005d81ff041622cf10e5bfd6836fd8ba
Ynda Jas <yndajas@gmail.com>
2023-08-15 15:47:24 +0100

    Add post helper to routeUtils

--- server/utils/routeUtils.ts
+++ server/utils/routeUtils.ts
@@ -5,6 +5,7 @@ import asyncMiddleware from '../middleware/asyncMiddleware'
 const actions = (router: Router) => {
   return {
     get: (path: string | Array<string>, handler: RequestHandler) => router.get(path, asyncMiddleware(handler)),
+    post: (path: string | Array<string>, handler: RequestHandler) => router.post(path, asyncMiddleware(handler)),
   }
 }
 
