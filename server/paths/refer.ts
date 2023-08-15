21740ad089c260da7fe79afdebba64f63f557e7d
Ynda Jas <yndajas@gmail.com>
2023-08-15 15:22:17 +0100

    Add paths for person search

--- server/paths/refer.ts
+++ server/paths/refer.ts
@@ -1,12 +1,17 @@
 import findPaths from './find'
 
 const startReferralPath = findPaths.offerings.show.path('refer')
+const newReferralPath = startReferralPath.path('new')
+
 const peoplePath = startReferralPath.path('people')
+const findPersonPath = peoplePath.path('search')
 const personPath = peoplePath.path(':prisonNumber')
 
 export default {
   start: startReferralPath,
+  new: newReferralPath,
   people: {
+    find: findPersonPath,
     show: personPath,
   },
 }
