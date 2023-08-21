f3e7cdf03fc3861f0be5aebdcd6491569db406ef
Ynda Jas <yndajas@gmail.com>
2023-08-21 14:51:13 +0100

    Add `CreatedReferralResponse` type
    For now, this is what we get back from the API when creating a referral.
    We'll need this to type the return object in the referral client's
    `create` method

--- server/@types/models/Referral.ts
+++ server/@types/models/Referral.ts
@@ -1,6 +1,12 @@
-export type Referral = {
+type Referral = {
   id: string // eslint-disable-next-line @typescript-eslint/member-ordering
   offeringId: string
   prisonNumber: string
   referrerId: string
 }
+
+type CreatedReferralResponse = {
+  referralId: Referral['id']
+}
+
+export type { CreatedReferralResponse, Referral }
