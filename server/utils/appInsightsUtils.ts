063d541114c09da71ae2e76bfd006bc4864e0d19
George Eaton <george@dxw.com>
2023-08-21 17:17:58 +0100

    Send username and activeCaseLoadId to AppInsights
    This will allow us to have more reliable user metrics not based on
    auto-generated user IDs, as the auto-generated user ID we currently see in
    Application Insights is attributing multiple users to the same user
    account when logging in e.g. in another browser/session.
    
    This is heavily based off [work done by the Use of Force
    team](https://github.com/ministryofjustice/use-of-force/pull/599/files)
    with a few more manual typings required, as well as me adding a bit more
    description to test cases.
    
    `addUserDataToRequests` should really be a private static function as
    it's not called outside this class, but with the amount of tricky
    optional behaviour going on here, I wanted to add some tests for it to
    make sure things were working correctly, and that appears to be how
    they've handled it on the Use of Force project too.

--- server/utils/appInsightsUtils.ts
+++ server/utils/appInsightsUtils.ts
@@ -1,13 +1,41 @@
 import type { TelemetryClient } from 'applicationinsights'
-import { DistributedTracingModes, defaultClient, setup } from 'applicationinsights'
+import { Contracts, DistributedTracingModes, defaultClient, setup } from 'applicationinsights'
+import type { EnvelopeTelemetry } from 'applicationinsights/out/Declarations/Contracts'
 
 import { buildNumber, packageData } from '../applicationVersion'
+import type { User } from '../data'
+
+type ContextObjectWithUser = {
+  res?: {
+    locals?: {
+      user: Partial<User>
+    }
+  }
+}
+
+export type ContextObjects = Record<string, ContextObjectWithUser> | undefined
 
 export default class AppInsightsUtils {
+  static addUserDataToRequests = (envelope: EnvelopeTelemetry, contextObjects: ContextObjects): boolean => {
+    const isRequest = envelope.data.baseType === Contracts.TelemetryTypeString.Request
+    const { username, activeCaseLoadId } = contextObjects?.['http.ServerRequest']?.res?.locals?.user || {}
+    if (isRequest && username && envelope.data.baseData) {
+      const { properties } = envelope.data.baseData
+      // eslint-disable-next-line no-param-reassign
+      envelope.data.baseData.properties = {
+        activeCaseLoadId,
+        username,
+        ...properties,
+      }
+    }
+    return true
+  }
+
   static buildClient = (name = AppInsightsUtils.defaultName()): TelemetryClient | null => {
     if (process.env.APPLICATIONINSIGHTS_CONNECTION_STRING) {
       defaultClient.context.tags['ai.cloud.role'] = name
       defaultClient.context.tags['ai.application.ver'] = this.version()
+      defaultClient.addTelemetryProcessor(AppInsightsUtils.addUserDataToRequests)
       return defaultClient
     }
     return null
