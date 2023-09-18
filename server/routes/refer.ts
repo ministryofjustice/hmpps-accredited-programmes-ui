5b0259410c608569e82d100eedde579f1f89e4fd
Ynda Jas <yndajas@gmail.com>
2023-09-18 18:50:01 +0100

    Add spacing in Refer routes
    Separate out related routes for easier scanning, while retaining roughly
    chronological order

--- server/routes/refer.ts
+++ server/routes/refer.ts
@@ -17,12 +17,16 @@ export default function routes(controllers: Controllers, router: Router): Router
   post(referPaths.create.pattern, referralsController.create())
   get(referPaths.show.pattern, referralsController.show())
   get(referPaths.showPerson.pattern, referralsController.showPerson())
+
   get(referPaths.confirmOasys.show.pattern, oasysConfirmationController.show())
   put(referPaths.confirmOasys.update.pattern, oasysConfirmationController.update())
+
   get(referPaths.reason.show.pattern, reasonController.show())
   put(referPaths.reason.update.pattern, reasonController.update())
+
   get(referPaths.hasCourseHistory.pattern, referralsController.hasCourseHistory())
   put(referPaths.updateHasCourseHistory.pattern, referralsController.updateHasCourseHistory())
+
   get(referPaths.checkAnswers.pattern, referralsController.checkAnswers())
   get(referPaths.complete.pattern, referralsController.complete())
   post(referPaths.submit.pattern, referralsController.submit())
