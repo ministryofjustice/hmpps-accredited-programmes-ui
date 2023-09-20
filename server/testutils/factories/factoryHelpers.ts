6941b13cb3cef997026f47cf699ebb2fbc9157d4
Ynda Jas <yndajas@gmail.com>
2023-09-20 15:50:39 +0100

    Add `optionalArrayElement` helper
    This wraps around `faker.helpers.arrayElement` (which returns a random
    element from an array), adding `undefined`. This can be used to generate
    values of types that are optional/could be `undefined`

--- server/testutils/factories/factoryHelpers.ts
+++ server/testutils/factories/factoryHelpers.ts
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker/locale/en_GB'
 import type { Factory } from 'fishery'
 
 export default class FactoryHelpers {
@@ -5,5 +6,10 @@ export default class FactoryHelpers {
     const quantity = Math.floor(Math.random() * options.max) + (options.min || 0)
 
     return factory.buildList(quantity)
+  }
+
+  static optionalArrayElement<T>(options: Array<T> | T): T | undefined {
+    const fullOptions: Array<T | undefined> = [undefined]
+    return faker.helpers.arrayElement(fullOptions.concat(options))
   }
 }
