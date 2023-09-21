6307706f0eff7a9aa8629179f4183b448f5cb3f7
John Robertson <john.robertson@digital.justice.gov.uk>
2023-09-21 14:35:39 +0100

    Update `RouteUtils` to use new `roleBasedAccessMiddleware`

--- server/utils/routeUtils.ts
+++ server/utils/routeUtils.ts
@@ -3,13 +3,18 @@
 import type { RequestHandler, Router } from 'express'
 
 import asyncMiddleware from '../middleware/asyncMiddleware'
+import roleBasedAccessMiddleware from '../middleware/roleBasedAccessMiddleware'
+import type { MiddlewareOptions } from '@accredited-programmes/server'
 
 export default class RouteUtils {
   static actions(router: Router) {
     return {
-      get: (path: Array<string> | string, handler: RequestHandler) => router.get(path, asyncMiddleware(handler)),
-      post: (path: Array<string> | string, handler: RequestHandler) => router.post(path, asyncMiddleware(handler)),
-      put: (path: Array<string> | string, handler: RequestHandler) => router.put(path, asyncMiddleware(handler)),
+      get: (path: Array<string> | string, handler: RequestHandler, middlewareOptions?: MiddlewareOptions) =>
+        router.get(path, asyncMiddleware(roleBasedAccessMiddleware(handler, middlewareOptions))),
+      post: (path: Array<string> | string, handler: RequestHandler, middlewareOptions?: MiddlewareOptions) =>
+        router.post(path, asyncMiddleware(roleBasedAccessMiddleware(handler, middlewareOptions))),
+      put: (path: Array<string> | string, handler: RequestHandler, middlewareOptions?: MiddlewareOptions) =>
+        router.put(path, asyncMiddleware(roleBasedAccessMiddleware(handler, middlewareOptions))),
     }
   }
 }
