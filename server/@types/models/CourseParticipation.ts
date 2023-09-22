e984d13b072ec852325c1baba9533afe57761451
Ynda Jas <yndajas@gmail.com>
2023-09-22 02:25:03 +0100

    Add `CourseParticipation` and related types
    The exported types will be needed in upcoming commits
    
    The `createdAt` property is a `string` rather than a `Date` in order to
    work as valid JSON in the client (change made retroactively after working
    on the client). If this approach continues to work, we can convert to a
    date in a util method if needed

---
+++ server/@types/models/CourseParticipation.ts
@@ -0,0 +1,35 @@
+import type { Course } from './Course'
+import type { Person } from './Person'
+
+type CourseParticipationOutcomeStatus = 'complete' | 'incomplete'
+
+type CourseParticipationOutcome = {
+  detail?: string
+  status?: CourseParticipationOutcomeStatus
+  yearCompleted?: number
+  yearStarted?: number
+}
+
+type CourseParticipationSetting = {
+  type: 'community' | 'custody'
+  location?: string
+}
+
+type CourseParticipation = {
+  id: string // eslint-disable-next-line @typescript-eslint/member-ordering
+  addedBy: string
+  createdAt: string
+  prisonNumber: Person['prisonNumber']
+  courseId?: Course['id']
+  otherCourseName?: string
+  outcome?: CourseParticipationOutcome
+  setting?: CourseParticipationSetting
+  source?: string
+}
+
+export type {
+  CourseParticipation,
+  CourseParticipationOutcome,
+  CourseParticipationOutcomeStatus,
+  CourseParticipationSetting,
+}
