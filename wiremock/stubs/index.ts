5daeeea010aa21fe5373fbd745df9748f5722076
Ynda Jas <yndajas@gmail.com>
2023-09-25 13:40:41 +0100

    Add referrals to local stubbed endpoints
    This stubs the GET/PUT referral endpoints for local development, while
    we await API updates
    
    The GET stub uses a course offering and prisoner from the local stubs
    and the AUTH_ADM user
    
    These stubbed endpoints will be used when running
    `script/server --mock-api`

--- wiremock/stubs/index.ts
+++ wiremock/stubs/index.ts
@@ -2,5 +2,6 @@ import courseOfferings from './courseOfferings.json'
 import courseParticipations from './courseParticipations.json'
 import courses from './courses.json'
 import prisoners from './prisoners.json'
+import referrals from './referrals.json'
 
-export { courseOfferings, courseParticipations, courses, prisoners }
+export { courseOfferings, courseParticipations, courses, prisoners, referrals }
