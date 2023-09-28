5ff7f84dbb6703e78b44e2328e5209225666a4b9
George Eaton <george@dxw.com>
2023-09-28 16:19:12 +0100

    Stub Prison API caseload endpoints
    It's been a bit complicated to get this running locally so I've decided
    to stub these endpoints locally for now.
    
    The stubbed caseloads are: Manchester, Brixton and Moorland prisons.

--- wiremock/stubs/index.ts
+++ wiremock/stubs/index.ts
@@ -1,7 +1,8 @@
+import caseloads from './caseloads.json'
 import courseOfferings from './courseOfferings.json'
 import courseParticipations from './courseParticipations.json'
 import courses from './courses.json'
 import prisoners from './prisoners.json'
 import referrals from './referrals.json'
 
-export { courseOfferings, courseParticipations, courses, prisoners, referrals }
+export { caseloads, courseOfferings, courseParticipations, courses, prisoners, referrals }
