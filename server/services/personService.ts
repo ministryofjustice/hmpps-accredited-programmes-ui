0809f00c1f239e98461eb49fe4bff1ad29d05fc0
George Eaton <george@dxw.com>
2023-09-30 18:51:02 +0100

    Update PersonService to use new endpoint

--- server/services/personService.ts
+++ server/services/personService.ts
@@ -4,6 +4,7 @@ import type { ResponseError } from 'superagent'
 import type { HmppsAuthClient, PrisonerClient, RestClientBuilder, RestClientBuilderWithoutToken } from '../data'
 import { PersonUtils } from '../utils'
 import type { Person } from '@accredited-programmes/models'
+import type { Caseload } from '@prison-api'
 
 export default class PersonService {
   constructor(
@@ -11,21 +12,27 @@ export default class PersonService {
     private readonly prisonerClientBuilder: RestClientBuilder<PrisonerClient>,
   ) {}
 
-  async getPerson(username: Express.User['username'], prisonNumber: string): Promise<Person | null> {
+  async getPerson(
+    username: Express.User['username'],
+    prisonNumber: Person['prisonNumber'],
+    caseloads: Array<Caseload>,
+  ): Promise<Person | null> {
     const hmppsAuthClient = this.hmppsAuthClientBuilder()
     const systemToken = await hmppsAuthClient.getSystemClientToken(username)
     const prisonerClient = this.prisonerClientBuilder(systemToken)
 
+    const caseloadIds = caseloads.map(caseload => caseload.caseLoadId)
+
     try {
-      const prisoner = await prisonerClient.find(prisonNumber)
+      const prisoner = await prisonerClient.find(prisonNumber, caseloadIds)
+
+      if (!prisoner) {
+        return null
+      }
 
       return PersonUtils.personFromPrisoner(prisoner)
     } catch (error) {
       const knownError = error as ResponseError
-
-      if (knownError.status === 404) {
-        return null
-      }
 
       throw createError(knownError.status || 500, knownError, {
         userMessage: `Error fetching prisoner ${prisonNumber}.`,
