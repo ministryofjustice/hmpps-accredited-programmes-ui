a00119dbce31cf409ac57420e4416dfe96c76173
Ynda Jas <yndajas@gmail.com>
2023-10-03 15:22:50 +0100

    Remove duplicate tests
    This appears to have been added by mistake, perhaps in a rebase

--- server/controllers/refer/courseParticipationsController.test.ts
+++ server/controllers/refer/courseParticipationsController.test.ts
@@ -186,54 +186,6 @@ describe('CourseParticipationsController', () => {
     })
   })
 
-  describe('index', () => {
-    const referral = referralFactory.build()
-
-    it("renders the index template for a person's programme history", async () => {
-      referralService.getReferral.mockResolvedValue(referral)
-
-      const person = personFactory.build()
-      personService.getPerson.mockResolvedValue(person)
-
-      const courseParticipations = [
-        courseParticipationFactory.build({ courseId: 'an-ID' }),
-        courseParticipationFactory.build({ courseId: undefined, otherCourseName: 'Another course' }),
-      ]
-      courseService.getParticipationsByPerson.mockResolvedValue(courseParticipations)
-
-      const course = courseFactory.build()
-      courseService.getCourse.mockResolvedValue(course)
-
-      const courseParticipationsWithNames = [
-        { ...courseParticipations[0], name: course.name },
-        { ...courseParticipations[1], name: 'Another course' },
-      ]
-      const summaryListsOptions = courseParticipationsWithNames.map(CourseParticipationUtils.summaryListOptions)
-
-      const requestHandler = courseParticipationsController.index()
-      await requestHandler(request, response, next)
-
-      expect(response.render).toHaveBeenCalledWith('referrals/courseParticipations/index', {
-        pageHeading: 'Accredited Programme history',
-        person,
-        referralId: referral.id,
-        summaryListsOptions,
-      })
-    })
-
-    describe('when the person service returns `null`', () => {
-      it('responds with a 404', async () => {
-        referralService.getReferral.mockResolvedValue(referral)
-        personService.getPerson.mockResolvedValue(null)
-
-        const requestHandler = courseParticipationsController.index()
-        const expectedError = createError(404)
-
-        expect(() => requestHandler(request, response, next)).rejects.toThrowError(expectedError)
-      })
-    })
-  })
-
   describe('new', () => {
     it('renders the new template for selecting a course/programme', async () => {
       const courses = courseFactory.buildList(2)
