54872869364b8e5018f73771900b87c9b8d4c543
John Robertson <john.robertson@digital.justice.gov.uk>
2023-10-17 19:52:43 +0100

    Add `@manage-users-api` types with `User` type
    Following the pattern of the other APIs have their own folder in @types.

---
+++ server/@types/manageUsersApi/index.d.ts
@@ -0,0 +1,10 @@
+export type User = {
+  active: boolean
+  authSource: string
+  name: string
+  userId: string
+  username: string
+  activeCaseLoadId?: string
+  staffId?: number
+  uuid?: string
+}
