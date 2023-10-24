93c535aafdf9f06b197ca24b654c9d01f7c20b5d
Ynda Jas <yndajas@gmail.com>
2023-10-24 09:59:17 +0100

    Fix typo in integration test description

--- integration_tests/e2e/refer/submit.cy.ts
+++ integration_tests/e2e/refer/submit.cy.ts
@@ -134,7 +134,7 @@ context('Submitting a referral', () => {
     })
 
     describe('for a person with no programme history', () => {
-      it('indicate that there is no programme history', () => {
+      it('indicates that there is no programme history', () => {
         cy.task('stubParticipationsByPerson', { courseParticipations: [], prisonNumber: prisoner.prisonerNumber })
 
         cy.visit(path)
