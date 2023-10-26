2c415063bfaf4071531fc29160b7f4535c3363ab
John Robertson <john.robertson@digital.justice.gov.uk>
2023-10-26 13:17:20 +0100

    Update `govukFormattedFullDateString` `datestring` param to be optional
    When no datestring is provided then use todays date.

--- server/utils/dateUtils.test.ts
+++ server/utils/dateUtils.test.ts
@@ -2,8 +2,19 @@ import DateUtils from './dateUtils'
 
 describe('DateUtils', () => {
   describe('govukFormattedFullDateString', () => {
-    it('returns a date string in the format specified by the GOV.UK style guide', () => {
-      expect(DateUtils.govukFormattedFullDateString('2001-06-03')).toEqual('3 June 2001')
+    it('returns todays date as a date string in the format specified by the GOV.UK style guide', () => {
+      const mockTodaysDate = new Date('2001-06-04')
+      jest.spyOn(global, 'Date').mockImplementation(() => mockTodaysDate)
+
+      expect(DateUtils.govukFormattedFullDateString()).toEqual('4 June 2001')
+
+      jest.spyOn(global, 'Date').mockRestore()
+    })
+
+    describe('when `datestring` is provided', () => {
+      it('returns the provided date as a date string in the format specified by the GOV.UK style guide', () => {
+        expect(DateUtils.govukFormattedFullDateString('2001-06-03')).toEqual('3 June 2001')
+      })
     })
   })
 })
