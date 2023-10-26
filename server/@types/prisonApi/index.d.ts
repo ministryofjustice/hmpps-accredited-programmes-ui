bfa93ade6621bd3ecdc5fd7772188871551e5041
Ynda Jas <yndajas@gmail.com>
2023-10-26 20:05:19 +0100

    Add sentence and offence details type
    This will be used as the return value for a Prison API endpoint. The
    endpoint returns more than this, but this is all we need for now

--- server/@types/prisonApi/index.d.ts
+++ server/@types/prisonApi/index.d.ts
@@ -1,7 +1,14 @@
-export type Caseload = {
+type Caseload = {
   caseLoadId: string // eslint-disable-next-line @typescript-eslint/member-ordering
   caseloadFunction: string
   currentlyActive: boolean
   description: string
   type: string
 }
+
+type SentenceAndOffenceDetails = {
+  sentenceDate: string
+  sentenceTypeDescription: string
+}
+
+export type { Caseload, SentenceAndOffenceDetails }
