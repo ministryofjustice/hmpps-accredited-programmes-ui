43263b2311c0f3aadd52bd3a82de93a5a6aa56b5
Ynda Jas <yndajas@gmail.com>
2023-10-26 17:40:15 +0100

    Add paths for sentence and offence information
    ... from the Prison API

--- server/paths/prisonApi.ts
+++ server/paths/prisonApi.ts
@@ -1,9 +1,14 @@
 import { path } from 'static-path'
 
-const caseloadsPath = path('/api/users/me/caseLoads')
+const apiBasePath = path('/api')
+const caseloadsPath = apiBasePath.path('users/me/caseLoads')
+const sentenceAndOffenceDetailsPath = apiBasePath.path('offender-sentences/booking/:bookingId/sentences-and-offences')
+const sentenceDatesPath = apiBasePath.path('bookings/:bookingId/sentenceDetail')
 
 export default {
   caseloads: {
     currentUser: caseloadsPath,
   },
+  sentenceAndOffenceDetails: sentenceAndOffenceDetailsPath,
+  sentenceDates: sentenceDatesPath,
 }
