c4d1df69b7c17b2c1cff44ac6ee6bb4a7bcaaee6
John Robertson <john.robertson@digital.justice.gov.uk>
2023-10-27 13:16:44 +0100

    Add `stubReferralWithScenario` and `stubSubmitReferral`
    We needed to stub multiple requests to get a referral when submitting a
    referral to mock the different states a referral could be in.
    
    Wiremock scenarios seems like the best option: https://wiremock.org/docs/stateful-behaviour/
    
    [Edit by Ynda] By default, the scenario state will be `"Started"`, so the
    first stub can use this as the `requiredScenarioState`. Providing
    `newScenarioState` updates the state when the stubbed endpoint is hit, which
    then allows stubs with different `requiredScenarioState`s to be matched on
    future calls

--- integration_tests/mockApis/referrals.ts
+++ integration_tests/mockApis/referrals.ts
@@ -4,6 +4,13 @@ import { apiPaths } from '../../server/paths'
 import { stubFor } from '../../wiremock'
 import type { Referral } from '@accredited-programmes/models'
 
+interface ReferralAndScenarioOptions {
+  referral: Referral
+  requiredScenarioState: string
+  scenarioName: string
+  newScenarioState?: string
+}
+
 export default {
   stubCreateReferral: (referral: Referral): SuperAgentRequest =>
     stubFor({
@@ -28,6 +35,39 @@ export default {
         headers: { 'Content-Type': 'application/json;charset=UTF-8' },
         jsonBody: referral,
         status: 200,
+      },
+    }),
+
+  stubReferralWithScenario: ({
+    newScenarioState,
+    referral,
+    requiredScenarioState,
+    scenarioName,
+  }: ReferralAndScenarioOptions): SuperAgentRequest =>
+    stubFor({
+      newScenarioState,
+      request: {
+        method: 'GET',
+        url: apiPaths.referrals.show({ referralId: referral.id }),
+      },
+      requiredScenarioState,
+      response: {
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: referral,
+        status: 200,
+      },
+      scenarioName,
+    }),
+
+  stubSubmitReferral: (referralId: Referral['id']): SuperAgentRequest =>
+    stubFor({
+      request: {
+        method: 'POST',
+        url: apiPaths.referrals.submit({ referralId }),
+      },
+      response: {
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        status: 204,
       },
     }),
 
