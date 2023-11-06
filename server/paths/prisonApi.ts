0cac2d3dc4f9668d5fab81d683bcd54d71514046
Ynda Jas <yndajas@gmail.com>
2023-11-06 17:47:48 +0000

    Remove sentence dates Prison API endpoint
    Per the previous commit, we no longer have a use for this since we'll
    get dates from the prisoner model

--- server/paths/prisonApi.ts
+++ server/paths/prisonApi.ts
@@ -3,12 +3,10 @@ import { path } from 'static-path'
 const apiBasePath = path('/api')
 const caseloadsPath = apiBasePath.path('users/me/caseLoads')
 const sentenceAndOffenceDetailsPath = apiBasePath.path('offender-sentences/booking/:bookingId/sentences-and-offences')
-const sentenceDatesPath = apiBasePath.path('bookings/:bookingId/sentenceDetail')
 
 export default {
   caseloads: {
     currentUser: caseloadsPath,
   },
   sentenceAndOffenceDetails: sentenceAndOffenceDetailsPath,
-  sentenceDates: sentenceDatesPath,
 }
