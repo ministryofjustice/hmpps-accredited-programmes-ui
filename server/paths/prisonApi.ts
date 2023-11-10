45e6dc0885b4b923778f474700e3cc36a7123430
John Robertson <john.robertson@digital.justice.gov.uk>
2023-11-10 09:37:07 +0000

    Add paths to prisonApi for getting offender and offence details

--- server/paths/prisonApi.ts
+++ server/paths/prisonApi.ts
@@ -2,11 +2,15 @@ import { path } from 'static-path'
 
 const apiBasePath = path('/api')
 const caseloadsPath = apiBasePath.path('users/me/caseLoads')
+const offenceCodePath = apiBasePath.path('offences/code/:offenceCode')
+const offenderPath = apiBasePath.path('bookings/offenderNo/:offenderNo')
 const sentenceAndOffenceDetailsPath = apiBasePath.path('offender-sentences/booking/:bookingId/sentences-and-offences')
 
 export default {
   caseloads: {
     currentUser: caseloadsPath,
   },
+  offenceCode: offenceCodePath,
+  offender: offenderPath,
   sentenceAndOffenceDetails: sentenceAndOffenceDetailsPath,
 }
