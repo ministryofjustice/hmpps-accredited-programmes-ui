615a08ac92d2eac9d30e79c1a2c22ae8f96bece6
John Robertson <john.robertson@digital.justice.gov.uk>
2023-11-10 15:58:19 +0000

    Add new Prison Api types for offence history page use
    Will be using these for the new prison api client methods for returning a persons offence history.

--- server/@types/prisonApi/index.d.ts
+++ server/@types/prisonApi/index.d.ts
@@ -2,6 +2,14 @@ import type { components } from './imported'
 
 type Caseload = components['schemas']['CaseLoad']
 
+type InmateDetail = components['schemas']['InmateDetail']
+
+type OffenceHistoryDetail = components['schemas']['OffenceHistoryDetail']
+
+type OffenceDto = components['schemas']['OffenceDto']
+
 type OffenderSentenceAndOffences = components['schemas']['OffenderSentenceAndOffences']
 
-export type { Caseload, OffenderSentenceAndOffences }
+type PageOffenceDto = components['schemas']['PageOffenceDto']
+
+export type { Caseload, InmateDetail, OffenceHistoryDetail, OffenceDto, OffenderSentenceAndOffences, PageOffenceDto }
