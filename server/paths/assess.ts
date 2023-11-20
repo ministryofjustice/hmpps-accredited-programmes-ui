a45ddfb29707c340fc9eec15eba488f678dd646a
John Robertson <john.robertson@digital.justice.gov.uk>
2023-11-20 14:35:23 +0000

    Add new path and route for case list

--- server/paths/assess.ts
+++ server/paths/assess.ts
@@ -1,9 +1,13 @@
 import { path } from 'static-path'
 
 const assessPathBase = path('/assess')
+const caseListPath = assessPathBase.path('referrals/case-list')
 const referralShowPathBase = assessPathBase.path('referrals/:referralId')
 
 export default {
+  caseList: {
+    show: caseListPath,
+  },
   show: {
     additionalInformation: referralShowPathBase.path('additional-information'),
     offenceHistory: referralShowPathBase.path('offence-history'),
