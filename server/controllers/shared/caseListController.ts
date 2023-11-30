6ce3ab6dc2decf39fd5c44f80196ea228cc09a77
Ynda Jas <yndajas@gmail.com>
2023-11-30 17:48:48 +0000

    Omit unused filters in case list URL
    Previously if you submitted the filter form from the case list, it would
    redirect to a URL with all the fields as query parameters even if they
    were empty. This omits empty query parameters to keep the URL clean

--- server/controllers/shared/caseListController.ts
+++ server/controllers/shared/caseListController.ts
@@ -1,10 +1,30 @@
 import type { Request, Response, TypedRequestHandler } from 'express'
 
+import { assessPaths } from '../../paths'
 import type { ReferralService } from '../../services'
-import { ReferralUtils, TypeUtils } from '../../utils'
+import { PathUtils, ReferralUtils, TypeUtils } from '../../utils'
+import type { QueryParam } from '@accredited-programmes/ui'
 
 export default class CaseListController {
   constructor(private readonly referralService: ReferralService) {}
+
+  filter(): TypedRequestHandler<Request, Response> {
+    return async (req: Request, res: Response) => {
+      TypeUtils.assertHasUser(req)
+
+      const queryParams: Array<QueryParam> = []
+
+      if (req.body.audience) {
+        queryParams.push({ key: 'audience', value: req.body.audience })
+      }
+
+      if (req.body.status) {
+        queryParams.push({ key: 'status', value: req.body.status })
+      }
+
+      return res.redirect(PathUtils.pathWithQuery(assessPaths.caseList.show({}), queryParams))
+    }
+  }
 
   show(): TypedRequestHandler<Request, Response> {
     return async (req: Request, res: Response) => {
@@ -20,6 +40,7 @@ export default class CaseListController {
       })
 
       return res.render('referrals/caseList/show', {
+        action: assessPaths.caseList.filter({}),
         audienceSelectItems: ReferralUtils.audienceSelectItems(audience),
         pageHeading: 'My referrals',
         referralStatusSelectItems: ReferralUtils.statusSelectItems(status),
