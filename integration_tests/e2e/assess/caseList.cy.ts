1a28ad58765c7f39d4fa26c7130194719e76757e
Ynda Jas <yndajas@gmail.com>
2023-12-13 18:17:20 +0000

    Simplify case list integration tests
    We shouldn't really need some of this specificity with the current
    test. 15 is the default page size, so the number of factory-built
    referral summaries is updated to match

--- integration_tests/e2e/assess/caseList.cy.ts
+++ integration_tests/e2e/assess/caseList.cy.ts
@@ -3,15 +3,9 @@ import { assessPaths } from '../../../server/paths'
 import { referralSummaryFactory } from '../../../server/testutils/factories'
 import Page from '../../pages/page'
 import { CaseListPage } from '../../pages/shared'
-import type { ReferralSummary } from '@accredited-programmes/models'
 
 context('Referral case lists', () => {
-  const referralSummaries: Array<ReferralSummary> = [
-    referralSummaryFactory.build({ prisonNumber: 'ABC123', status: 'assessment_started' }),
-    referralSummaryFactory.build({ prisonNumber: 'ABC789', status: 'referral_started' }),
-    referralSummaryFactory.build({ prisonNumber: 'ABC456', status: 'awaiting_assessment' }),
-    referralSummaryFactory.build({ prisonNumber: 'ABC000', status: 'referral_submitted' }),
-  ]
+  const referralSummaries = referralSummaryFactory.buildList(15)
 
   beforeEach(() => {
     cy.task('reset')
@@ -49,11 +43,7 @@ context('Referral case lists', () => {
       const programmeStrandSelectedValue = 'general offence'
       const referralStatusSelectedValue = 'assessment started'
       const filteredReferralSummaries = [
-        referralSummaryFactory.build({
-          audiences: ['General offence'],
-          prisonNumber: 'ABC123',
-          status: 'assessment_started',
-        }),
+        referralSummaryFactory.build({ audiences: ['General offence'], status: 'assessment_started' }),
       ]
 
       caseListPage.shouldFilter(programmeStrandSelectedValue, referralStatusSelectedValue, filteredReferralSummaries)
