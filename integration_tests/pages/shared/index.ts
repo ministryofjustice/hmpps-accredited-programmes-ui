410aeab42b3ca53c0f532a74fabd08fefcc42d00
John Robertson <john.robertson@digital.justice.gov.uk>
2024-01-11 11:20:50 +0000

    Add `OffenceAnalysisPage` for integration tests
    To be used to check the content of the Section 2 - Offence Analysis page when viewing the risks and needs on a referral.

--- integration_tests/pages/shared/index.ts
+++ integration_tests/pages/shared/index.ts
@@ -3,11 +3,13 @@ import AdditionalInformationPage from './showReferral/additionalInformation'
 import OffenceHistoryPage from './showReferral/offenceHistory'
 import PersonalDetailsPage from './showReferral/personalDetails'
 import ProgrammeHistoryPage from './showReferral/programmeHistory'
+import OffenceAnalysisPage from './showReferral/risksAndNeeds/offenceAnalysis'
 import SentenceInformationPage from './showReferral/sentenceInformation'
 
 export {
   AdditionalInformationPage,
   CaseListPage,
+  OffenceAnalysisPage,
   OffenceHistoryPage,
   PersonalDetailsPage,
   ProgrammeHistoryPage,
