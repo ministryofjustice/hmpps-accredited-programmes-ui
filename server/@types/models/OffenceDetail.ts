a84d7b0dbafe74a2b911377469ec77d8acd64e1a
John Robertson <john.robertson@digital.justice.gov.uk>
2024-01-12 11:23:50 +0000

    Update `OffenceDetail` type to make keys optional
    Generated manually from the api schema using openapi-typescript.

--- server/@types/models/OffenceDetail.ts
+++ server/@types/models/OffenceDetail.ts
@@ -1,18 +1,18 @@
 export interface OffenceDetail {
-  acceptsResponsibility: boolean
-  acceptsResponsibilityDetail: string
-  contactTargeting: boolean
-  domesticViolence: boolean
-  motivationAndTriggers: string
-  numberOfOthersInvolved: number
-  offenceDetails: string
-  othersInvolvedDetail: string
-  patternOffending: string
-  peerGroupInfluences: string
-  raciallyMotivated: boolean
-  recognisesImpact: boolean
-  repeatVictimisation: boolean
-  revenge: boolean
-  stalking: boolean
-  victimWasStranger: boolean
+  acceptsResponsibility?: boolean
+  acceptsResponsibilityDetail?: string
+  contactTargeting?: boolean
+  domesticViolence?: boolean
+  motivationAndTriggers?: string
+  numberOfOthersInvolved?: number
+  offenceDetails?: string
+  othersInvolvedDetail?: string
+  patternOffending?: string
+  peerGroupInfluences?: string
+  raciallyMotivated?: boolean
+  recognisesImpact?: boolean
+  repeatVictimisation?: boolean
+  revenge?: boolean
+  stalking?: boolean
+  victimWasStranger?: boolean
 }
