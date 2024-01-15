81b070192bebe2ca1a2f34d9bac8722b18913820
Ynda Jas <yndajas@gmail.com>
2024-01-15 18:28:01 +0000

    Make optional fields optional in factory
    The `OffenceDetailFactory` currently doesn't reflect the optionality of
    the `OffenceDetail` fields. This doesn't appear to require any test
    changes at present, but if we add in `OasysClient` tests later we'll
    likely need to add a `withAllOptionalFields` trait to avoid
    unpredictable nullish data
    
    This also adds a `withAllOptionalFields` trait, similar to other
    factories, for when we want to build an object with no nullish optional
    fields. This is used in a controller test to ensure that `when`-based
    mocking for `OffenceAnalysisUtils.textValue` still works. We use `when`
    to mock the return value of a method when passed particular arguments:
    if two fields are both `null`, the second `when` `null` mock would
    overwrite the first, which creates unpredictability

--- server/testutils/factories/offenceDetail.ts
+++ server/testutils/factories/offenceDetail.ts
@@ -1,25 +1,47 @@
 import { faker } from '@faker-js/faker'
 import { Factory } from 'fishery'
 
+import FactoryHelpers from './factoryHelpers'
 import type { OffenceDetail } from '@accredited-programmes/models'
 
-export default Factory.define<OffenceDetail>(() => {
-  return {
-    acceptsResponsibility: faker.datatype.boolean(),
-    acceptsResponsibilityDetail: faker.lorem.sentence(),
-    contactTargeting: faker.datatype.boolean(),
-    domesticViolence: faker.datatype.boolean(),
-    motivationAndTriggers: faker.lorem.sentence(),
-    numberOfOthersInvolved: faker.number.int(),
-    offenceDetails: faker.lorem.sentence(),
-    othersInvolvedDetail: faker.lorem.sentence(),
-    patternOffending: faker.lorem.sentence(),
-    peerGroupInfluences: faker.lorem.sentence(),
-    raciallyMotivated: faker.datatype.boolean(),
-    recognisesImpact: faker.datatype.boolean(),
-    repeatVictimisation: faker.datatype.boolean(),
-    revenge: faker.datatype.boolean(),
-    stalking: faker.datatype.boolean(),
-    victimWasStranger: faker.datatype.boolean(),
+class OffenceDetailFactory extends Factory<OffenceDetail> {
+  withAllOptionalFields() {
+    return this.params({
+      acceptsResponsibility: faker.datatype.boolean(),
+      acceptsResponsibilityDetail: faker.lorem.sentence(),
+      contactTargeting: faker.datatype.boolean(),
+      domesticViolence: faker.datatype.boolean(),
+      motivationAndTriggers: faker.lorem.sentence(),
+      numberOfOthersInvolved: faker.number.int(),
+      offenceDetails: faker.lorem.sentence(),
+      othersInvolvedDetail: faker.lorem.sentence(),
+      patternOffending: faker.lorem.sentence(),
+      peerGroupInfluences: faker.lorem.sentence(),
+      raciallyMotivated: faker.datatype.boolean(),
+      recognisesImpact: faker.datatype.boolean(),
+      repeatVictimisation: faker.datatype.boolean(),
+      revenge: faker.datatype.boolean(),
+      stalking: faker.datatype.boolean(),
+    })
   }
-})
+}
+
+export default OffenceDetailFactory.define(
+  (): OffenceDetail => ({
+    acceptsResponsibility: FactoryHelpers.optionalArrayElement(faker.datatype.boolean()),
+    acceptsResponsibilityDetail: FactoryHelpers.optionalArrayElement(faker.lorem.sentence()),
+    contactTargeting: FactoryHelpers.optionalArrayElement(faker.datatype.boolean()),
+    domesticViolence: FactoryHelpers.optionalArrayElement(faker.datatype.boolean()),
+    motivationAndTriggers: FactoryHelpers.optionalArrayElement(faker.lorem.sentence()),
+    numberOfOthersInvolved: FactoryHelpers.optionalArrayElement(faker.number.int()),
+    offenceDetails: FactoryHelpers.optionalArrayElement(faker.lorem.sentence()),
+    othersInvolvedDetail: FactoryHelpers.optionalArrayElement(faker.lorem.sentence()),
+    patternOffending: FactoryHelpers.optionalArrayElement(faker.lorem.sentence()),
+    peerGroupInfluences: FactoryHelpers.optionalArrayElement(faker.lorem.sentence()),
+    raciallyMotivated: FactoryHelpers.optionalArrayElement(faker.datatype.boolean()),
+    recognisesImpact: FactoryHelpers.optionalArrayElement(faker.datatype.boolean()),
+    repeatVictimisation: FactoryHelpers.optionalArrayElement(faker.datatype.boolean()),
+    revenge: FactoryHelpers.optionalArrayElement(faker.datatype.boolean()),
+    stalking: FactoryHelpers.optionalArrayElement(faker.datatype.boolean()),
+  }),
+)
