11f6fbb6f6047f31bcdedb7ca363fb6292de426a
John Robertson <john.robertson@digital.justice.gov.uk>
2024-01-19 12:01:24 +0000

    Add `findRelationships` to `OasysClient` and related api path

--- server/data/accreditedProgrammesApi/oasysClient.ts
+++ server/data/accreditedProgrammesApi/oasysClient.ts
@@ -2,7 +2,7 @@
 import config, { type ApiConfig } from '../../config'
 import { apiPaths } from '../../paths'
 import RestClient from '../restClient'
-import type { Lifestyle, OffenceDetail, Referral, RoshAnalysis } from '@accredited-programmes/models'
+import type { Lifestyle, OffenceDetail, Referral, Relationships, RoshAnalysis } from '@accredited-programmes/models'
 import type { SystemToken } from '@hmpps-auth'
 
 export default class OasysClient {
@@ -22,6 +22,12 @@ export default class OasysClient {
     return (await this.restClient.get({
       path: apiPaths.oasys.offenceDetails({ prisonNumber }),
     })) as OffenceDetail
+  }
+
+  async findRelationships(prisonNumber: Referral['prisonNumber']): Promise<Relationships> {
+    return (await this.restClient.get({
+      path: apiPaths.oasys.relationships({ prisonNumber }),
+    })) as Relationships
   }
 
   async findRoshAnalysis(prisonNumber: Referral['prisonNumber']): Promise<RoshAnalysis> {
--- server/paths/api.ts
+++ server/paths/api.ts
@@ -34,6 +34,7 @@ export default {
   oasys: {
     lifestyle: oasysBasePath.path('lifestyle'),
     offenceDetails: oasysBasePath.path('offence-details'),
+    relationships: oasysBasePath.path('relationships'),
     roshAnalysis: oasysBasePath.path('rosh-analysis'),
   },
   offerings: {
