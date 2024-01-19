4679f1a13b74aada65813e02b27c7065ae8fdf64
John Robertson <john.robertson@digital.justice.gov.uk>
2024-01-19 14:48:45 +0000

    Add `RelationshipsPage` for integration tests

--- integration_tests/pages/shared/index.ts
+++ integration_tests/pages/shared/index.ts
@@ -5,6 +5,7 @@ import PersonalDetailsPage from './showReferral/personalDetails'
 import ProgrammeHistoryPage from './showReferral/programmeHistory'
 import LifestyleAndAssociatesPage from './showReferral/risksAndNeeds/lifestyleAndAssociates'
 import OffenceAnalysisPage from './showReferral/risksAndNeeds/offenceAnalysis'
+import RelationshipsPage from './showReferral/risksAndNeeds/relationships'
 import RoshAnalysisPage from './showReferral/risksAndNeeds/roshAnalysis'
 import SentenceInformationPage from './showReferral/sentenceInformation'
 
@@ -16,6 +17,7 @@ export {
   OffenceHistoryPage,
   PersonalDetailsPage,
   ProgrammeHistoryPage,
+  RelationshipsPage,
   RoshAnalysisPage,
   SentenceInformationPage,
 }
---
+++ integration_tests/pages/shared/showReferral/risksAndNeeds/relationships.ts
@@ -0,0 +1,36 @@
+import { CourseUtils, RelationshipsUtils } from '../../../../../server/utils'
+import Page from '../../../page'
+import type { Course, Relationships } from '@accredited-programmes/models'
+
+export default class RelationshipsPage extends Page {
+  relationships: Relationships
+
+  constructor(args: { course: Course; relationships: Relationships }) {
+    const { course, relationships } = args
+
+    const coursePresenter = CourseUtils.presentCourse(course)
+
+    super(`Referral to ${coursePresenter.nameAndAlternateName}`)
+
+    this.relationships = relationships
+  }
+
+  shouldContainDomesticViolenceSummaryList() {
+    cy.get('[data-testid="domestic-violence-summary-list"]').then(summaryListElement => {
+      this.shouldContainSummaryListRows(
+        RelationshipsUtils.domesticViolenceSummaryListRows(this.relationships),
+        summaryListElement,
+      )
+    })
+  }
+
+  shouldContainNoRelationshipsSummaryCard() {
+    cy.get('[data-testid="no-relationships-summary-card"]').then(summaryCardElement => {
+      this.shouldContainKeylessSummaryCard(
+        'Section 6 - Relationships',
+        'No relationships found in OASys. Add relationships to OASys to see it here.',
+        summaryCardElement,
+      )
+    })
+  }
+}
