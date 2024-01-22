4bfe3f6440d7514d0fb7880b9c0946d29a8fc55c
George Eaton <george@dxw.com>
2024-01-22 12:25:34 +0000

    Add `EmotionalWellbeingPage` for integration tests

---
+++ integration_tests/pages/shared/showReferral/risksAndNeeds/emotionalWellbeing.ts
@@ -0,0 +1,36 @@
+import { CourseUtils, EmotionalWellbeingUtils } from '../../../../../server/utils'
+import Page from '../../../page'
+import type { Course, Psychiatric } from '@accredited-programmes/models'
+
+export default class EmotionalWellbeing extends Page {
+  psychiatric: Psychiatric
+
+  constructor(args: { course: Course; psychiatric: Psychiatric }) {
+    const { course, psychiatric } = args
+
+    const coursePresenter = CourseUtils.presentCourse(course)
+
+    super(`Referral to ${coursePresenter.nameAndAlternateName}`)
+
+    this.psychiatric = psychiatric
+  }
+
+  shouldContainNoPsychiatricDataSummaryCard() {
+    cy.get('[data-testid="no-psychiatric-data-summary-card"]').then(summaryCardElement => {
+      this.shouldContainKeylessSummaryCard(
+        'Section 10.6 - Psychiatric problems',
+        'No psychiatric data found in OASys. Add psychiatric data to OASys to see them here.',
+        summaryCardElement,
+      )
+    })
+  }
+
+  shouldContainPsychiatricProblemsSummaryList() {
+    cy.get('[data-testid="psychiatric-problems-summary-list"]').then(summaryListElement => {
+      this.shouldContainSummaryListRows(
+        EmotionalWellbeingUtils.psychiatricSummaryListRows(this.psychiatric),
+        summaryListElement,
+      )
+    })
+  }
+}
