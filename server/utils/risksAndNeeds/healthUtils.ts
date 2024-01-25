384439515fa69b458475c778b0bd527d25b39fdd
John Robertson <john.robertson@digital.justice.gov.uk>
2024-01-25 21:06:07 +0000

    Add `HealthUtils`

---
+++ server/utils/risksAndNeeds/healthUtils.ts
@@ -0,0 +1,20 @@
+import ShowRisksAndNeedsUtils from '../referrals/showRisksAndNeedsUtils'
+import type { Health } from '@accredited-programmes/models'
+import type { GovukFrontendSummaryListRowWithKeyAndValue } from '@accredited-programmes/ui'
+
+export default class HealthUtils {
+  static healthSummaryListRows(health: Health): Array<GovukFrontendSummaryListRowWithKeyAndValue> {
+    let generalHealthTextString = ShowRisksAndNeedsUtils.yesOrNo(health.anyHealthConditions)
+
+    if (health.anyHealthConditions) {
+      generalHealthTextString += `<br><br>${ShowRisksAndNeedsUtils.textValue(health.description)}`
+    }
+
+    return [
+      {
+        key: { text: 'General health - any physical or mental health conditions? (optional)' },
+        value: { html: generalHealthTextString },
+      },
+    ]
+  }
+}
