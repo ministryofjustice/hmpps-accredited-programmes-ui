6a3b87f338ef4daa28d3238020f65e0ac3e01a70
Ynda Jas <yndajas@gmail.com>
2024-01-31 11:03:40 +0000

    Remove redundant Nunjucks component paths
    We don't import components directly from these paths: we always import
    from the higher-level paths in templates which provides clearer
    namespacing
    
    Per: https://github.com/ministryofjustice/hmpps-template-typescript/commit/b54a9f7721247345ec3621be68d8a6c2543d545f

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -41,9 +41,7 @@ export default function nunjucksSetup(app: express.Express, path: pathModule.Pla
     [
       path.join(__dirname, '../../server/views'),
       'node_modules/govuk-frontend/dist/',
-      'node_modules/govuk-frontend/dist/govuk/components/',
       'node_modules/@ministryofjustice/frontend/',
-      'node_modules/@ministryofjustice/frontend/moj/components/',
     ],
     {
       autoescape: true,
