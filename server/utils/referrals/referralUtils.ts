4281fdc0e6d78bd7f066de92e036df66592e77ac
John Robertson <john.robertson@digital.justice.gov.uk>
2024-02-27 09:03:14 +0000

    Update `statusCategoriesToRadioItems` to mark a radio item as checked if a matching value is provided

--- server/utils/referrals/referralUtils.ts
+++ server/utils/referrals/referralUtils.ts
@@ -2,13 +2,17 @@ import type { ReferralStatusCategory } from '@accredited-programmes/models'
 import type { GovukFrontendRadiosItem } from '@govuk-frontend'
 
 export default class ReferralUtils {
-  static statusCategoriesToRadioItems(statusCategories: Array<ReferralStatusCategory>): Array<GovukFrontendRadiosItem> {
+  static statusCategoriesToRadioItems(
+    statusCategories: Array<ReferralStatusCategory>,
+    selectedStatusCategory?: ReferralStatusCategory['code'],
+  ): Array<GovukFrontendRadiosItem> {
     return statusCategories.reduce<Array<GovukFrontendRadiosItem>>((acc, statusCategory) => {
       if (statusCategory.description === 'Other') {
         acc.push({ divider: 'or', value: '' })
       }
 
       acc.push({
+        checked: selectedStatusCategory === statusCategory.code,
         text: statusCategory.description,
         value: statusCategory.code,
       })
