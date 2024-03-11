e31a5c87d3910755818631b1dbd7c9c155d418be
John Robertson <john.robertson@digital.justice.gov.uk>
2024-03-11 11:03:55 +0000

    Add `organisationEnabled` to `CourseOffering` type
    Also update associated test util.

--- server/@types/models/CourseOffering.ts
+++ server/@types/models/CourseOffering.ts
@@ -1,6 +1,7 @@
 export type CourseOffering = {
   id: string // eslint-disable-next-line @typescript-eslint/member-ordering
   contactEmail: string
+  organisationEnabled: boolean
   organisationId: string
   referable: boolean
   secondaryContactEmail: string | null
