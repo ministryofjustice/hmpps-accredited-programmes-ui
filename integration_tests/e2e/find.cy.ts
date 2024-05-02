c0593558a22cd7b36508d98a2da89a4f4390f900
John Robertson <john.robertson@digital.justice.gov.uk>
2024-05-02 20:42:46 +0100

    Add text to course page when there are no offerings available

--- integration_tests/e2e/find.cy.ts
+++ integration_tests/e2e/find.cy.ts
@@ -62,6 +62,23 @@ context('Find', () => {
       coursePage.shouldHaveOrganisations(organisationsWithOfferingIds)
     })
 
+    describe('when there are no offerings for a course', () => {
+      it('shows a message that there are no offerings', () => {
+        cy.task('stubCourse', courses[0])
+
+        cy.task('stubOfferingsByCourse', { courseId: courses[0].id, courseOfferings: [] })
+
+        const path = findPaths.show({ courseId: courses[0].id })
+        cy.visit(path)
+
+        const coursePage = Page.verifyOnPage(CoursePage, courses[0])
+        coursePage.shouldContainBackLink(findPaths.index({}))
+        coursePage.shouldContainHomeLink()
+        coursePage.shouldHaveCourse()
+        coursePage.shouldContainNoOfferingsText()
+      })
+    })
+
     describe('Viewing a single offering', () => {
       it('shows a single offering with no secondary email address', () => {
         const courseOffering = courseOfferingFactory.build({
