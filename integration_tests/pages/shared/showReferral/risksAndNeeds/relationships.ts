bd9e81c3e39d7693a520072baffc52bf675c0266
John Robertson <john.robertson@digital.justice.gov.uk>
2024-08-15 11:48:07 +0100

    Use `Relationships` type from imported file

--- integration_tests/pages/shared/showReferral/risksAndNeeds/relationships.ts
+++ integration_tests/pages/shared/showReferral/risksAndNeeds/relationships.ts
@@ -1,6 +1,7 @@
 import { CourseUtils, RelationshipsUtils, ShowRisksAndNeedsUtils } from '../../../../../server/utils'
 import Page from '../../../page'
-import type { Course, Relationships } from '@accredited-programmes/models'
+import type { Course } from '@accredited-programmes/models'
+import type { Relationships } from '@accredited-programmes-api'
 
 export default class RelationshipsPage extends Page {
   relationships: Relationships
