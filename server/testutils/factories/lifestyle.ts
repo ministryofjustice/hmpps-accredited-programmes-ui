983033c60d4c84335536c1e36e8bc678416c4eac
John Robertson <john.robertson@digital.justice.gov.uk>
2024-08-16 08:09:53 +0100

    Update lifestyle test factory to return value for `easilyInfluenced`

--- server/testutils/factories/lifestyle.ts
+++ server/testutils/factories/lifestyle.ts
@@ -2,11 +2,12 @@ import { faker } from '@faker-js/faker'
 import { Factory } from 'fishery'
 
 import FactoryHelpers from './factoryHelpers'
-import type { Lifestyle } from '@accredited-programmes/models'
+import type { Lifestyle } from '@accredited-programmes-api'
 
 export default Factory.define<Lifestyle>(() => {
   return {
     activitiesEncourageOffending: FactoryHelpers.optionalArrayElement(faker.lorem.sentence()),
+    easilyInfluenced: FactoryHelpers.optionalArrayElement(faker.lorem.sentence()),
     lifestyleIssues: FactoryHelpers.optionalArrayElement(faker.lorem.sentence()),
   }
 })
