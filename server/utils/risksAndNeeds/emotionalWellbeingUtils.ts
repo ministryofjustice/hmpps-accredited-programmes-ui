208d927b6ba3fb53dc328a6d4e3e2690584e0525
John Robertson <john.robertson@digital.justice.gov.uk>
2024-08-19 09:15:21 +0100

    Update `EmotionalWellbeingUtils.psychiatricSummaryListRows` to display `difficultiesCoping` value

--- server/utils/risksAndNeeds/emotionalWellbeingUtils.ts
+++ server/utils/risksAndNeeds/emotionalWellbeingUtils.ts
@@ -1,10 +1,14 @@
 import ShowRisksAndNeedsUtils from '../referrals/showRisksAndNeedsUtils'
-import type { Psychiatric } from '@accredited-programmes/models'
 import type { GovukFrontendSummaryListRowWithKeyAndValue } from '@accredited-programmes/ui'
+import type { Psychiatric } from '@accredited-programmes-api'
 
 export default class EmotionalWellbeingUtils {
   static psychiatricSummaryListRows(psychiatric: Psychiatric): Array<GovukFrontendSummaryListRowWithKeyAndValue> {
     return [
+      {
+        key: { text: '10.1 - Difficulties coping' },
+        value: { text: ShowRisksAndNeedsUtils.textValue(psychiatric.difficultiesCoping) },
+      },
       {
         key: { text: '10.2 - Current psychological problems or depression' },
         value: { text: ShowRisksAndNeedsUtils.textValue(psychiatric.currPsychologicalProblems) },
