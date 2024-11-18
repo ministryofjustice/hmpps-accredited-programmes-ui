fadd8f6fb57629af2aed2b6563184df848c66fd7
John Robertson <john.robertson@digital.justice.gov.uk>
2024-11-18 10:00:45 +0000

    Remove roll out notification banner from homepage
    In preparation for rolling out to all locations.

--- integration_tests/e2e/app.cy.ts
+++ integration_tests/e2e/app.cy.ts
@@ -36,26 +36,6 @@ context('App', () => {
       })
     })
 
-    it('Shows a notification banner on the index page', () => {
-      const bannerInnerHTML = `
-        <h3 class="govuk-notification-banner__heading">Rolling out the referral service</h3>
-        <p class="govuk-body">This is a new service. While we're rolling it out, referrers can only use it to refer people to Accredited Programmes at a few prisons. At the moment, you can refer to:</p>
-        <ul class="govuk-list govuk-list--bullet">
-          <li>Drake Hall (HMP & YOI)</li>
-          <li>Fosse Way (HMP & YOI)</li>
-          <li>Long Lartin (HMP)</li>
-          <li>Onley (HMP)</li>
-          <li>Peterborough Female (HMP & YOI)</li>
-          <li>Peterborough (HMP & YOI)</li>
-          <li>Stocken (HMP)</li>
-          <li>Whatton (HMP)</li>
-        </ul>
-        <p class="govuk-body">You can still refer to other prisons using your usual process.</p>
-      `
-      const indexPage = Page.verifyOnPage(IndexPage)
-      indexPage.shouldContainNotificationBanner('Important', bannerInnerHTML)
-    })
-
     it('Shows the correct navigation elements', () => {
       const indexPage = Page.verifyOnPage(IndexPage)
       indexPage.shouldNotContainNavigation()
