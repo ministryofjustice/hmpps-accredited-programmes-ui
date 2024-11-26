6a345a6c0f49f8f9cafd7669a5cb4f288849a84e
John Robertson <jsrobertson@users.noreply.github.com>
2024-11-26 14:21:39 +0000

    Revert "(APG-554) Show error specific content when the pathway is not known"

--- server/utils/risksAndNeeds/pniUtils.test.ts
+++ server/utils/risksAndNeeds/pniUtils.test.ts
@@ -109,26 +109,15 @@ describe('PniUtils', () => {
       })
     })
 
-    it('returns the pathway content for the given person name and MISSING_INFORMATION programme pathway', () => {
-      const pathwayContent = PniUtils.pathwayContent(personName, 'MISSING_INFORMATION')
+    it('returns the pathway content for the given person name and an unknown programme pathway', () => {
+      const pathwayContent = PniUtils.pathwayContent(personName)
 
       expect(pathwayContent).toEqual({
         bodyText:
           'There is not enough information in the layer 3 assessment to calculate the recommended programme pathway.',
         class: 'pathway-content--missing',
-        dataTestId: 'missing-informaton-pathway-content',
+        dataTestId: 'unknown-pathway',
         headingText: 'Information missing',
-      })
-    })
-
-    it('returns the error pathway content when the pathway is undefined', () => {
-      const pathwayContent = PniUtils.pathwayContent(personName)
-
-      expect(pathwayContent).toEqual({
-        bodyText: 'The service cannot calculate the recommended pathway at the moment. Try again later.',
-        class: 'pathway-content--error',
-        dataTestId: 'error-pathway-content',
-        headingText: 'Error',
       })
     })
   })
