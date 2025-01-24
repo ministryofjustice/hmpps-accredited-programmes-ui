7cc04d25496815ed9fa9c1057b6d929230ae9679
John Robertson <john.robertson@digital.justice.gov.uk>
2025-01-24 09:28:59 +0000

    Add cypress tests for new person search for PNI Find journey

--- integration_tests/pages/find/index.ts
+++ integration_tests/pages/find/index.ts
@@ -1,5 +1,6 @@
 import CoursePage from './course'
 import CourseOfferingPage from './courseOffering'
 import CoursesPage from './courses'
+import PersonSearchPage from './personSearch'
 
-export { CourseOfferingPage, CoursePage, CoursesPage }
+export { CourseOfferingPage, CoursePage, CoursesPage, PersonSearchPage }
