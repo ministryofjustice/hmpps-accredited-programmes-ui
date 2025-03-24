bee42003f8b8360bb801830d265553f06ab5c4a4
John Robertson <john.robertson@digital.justice.gov.uk>
2025-03-24 14:56:01 +0000

    Add `submit` method to `UpdateLdcController`
    Also includes update to template to handle error messages.

--- server/controllers/assess/updateLdcController.ts
+++ server/controllers/assess/updateLdcController.ts
@@ -2,7 +2,7 @@ import type { Request, Response, TypedRequestHandler } from 'express'
 
 import { assessPaths } from '../../paths'
 import type { PersonService, ReferralService } from '../../services'
-import { TypeUtils } from '../../utils'
+import { FormUtils, TypeUtils } from '../../utils'
 
 export default class UpdateLdcController {
   constructor(
@@ -20,12 +20,37 @@ export default class UpdateLdcController {
       const referral = await this.referralService.getReferral(username, referralId)
       const person = await this.personService.getPerson(username, referral.prisonNumber)
 
+      FormUtils.setFieldErrors(req, res, ['ldcReason'])
+
       return res.render('referrals/updateLdc/show', {
         backLinkHref: assessPaths.show.personalDetails({ referralId }),
         hasLdc: referral.hasLdc,
         pageHeading: 'Update Learning disabilities and challenges (LDC)',
         person,
       })
+    }
+  }
+
+  submit(): TypedRequestHandler<Request, Response> {
+    return async (req: Request, res: Response) => {
+      TypeUtils.assertHasUser(req)
+
+      const { referralId } = req.params
+      const { username } = req.user
+
+      if (!req.body.ldcReason) {
+        req.flash('ldcReasonError', 'Select a reason for updating the learning disabilities and challenges status')
+        return res.redirect(assessPaths.updateLdc.show({ referralId }))
+      }
+
+      const referral = await this.referralService.getReferral(username, referralId)
+
+      await this.referralService.updateReferral(username, referralId, {
+        ...referral,
+        hasLdcBeenOverriddenByProgrammeTeam: true,
+      })
+
+      return res.redirect(assessPaths.show.personalDetails({ referralId }))
     }
   }
 }
