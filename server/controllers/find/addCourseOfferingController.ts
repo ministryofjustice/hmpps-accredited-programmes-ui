738afe1b60eaf7cdc189a40fa69189ceb4015013
Francis O'Brien <francis.obrien@digital.justice.gov.uk>
2025-05-09 14:29:21 +0100

    Add page to add BC course offering
    fix back link

--- server/controllers/find/addCourseOfferingController.ts
+++ server/controllers/find/addCourseOfferingController.ts
@@ -2,7 +2,7 @@ import type { Request, Response, TypedRequestHandler } from 'express'
 
 import { findPaths } from '../../paths'
 import type { CourseService, OrganisationService } from '../../services'
-import { OrganisationUtils, TypeUtils } from '../../utils'
+import { CourseUtils, OrganisationUtils, TypeUtils } from '../../utils'
 
 export default class AddCourseOfferingController {
   constructor(
@@ -16,8 +16,8 @@ export default class AddCourseOfferingController {
 
       const { courseId } = req.params
 
+      const course = await this.courseService.getCourse(req.user.username, courseId)
       const organisations = await this.organisationService.getAllOrganisations(req.user.token)
-
       const organisationSelectItems = OrganisationUtils.organisationSelectItems(organisations)
 
       res.render('courses/offerings/form/show', {
@@ -25,6 +25,7 @@ export default class AddCourseOfferingController {
         backLinkHref: findPaths.show({ courseId }),
         organisationSelectItems,
         pageHeading: 'Add a Location',
+        showBuildingChoicesOptions: CourseUtils.isBuildingChoices(course.displayName),
       })
     }
   }
@@ -32,12 +33,50 @@ export default class AddCourseOfferingController {
   submit(): TypedRequestHandler<Request, Response> {
     return async (req: Request, res: Response) => {
       TypeUtils.assertHasUser(req)
-
       const { courseId } = req.params
-      const { contactEmail, organisationId, referable, secondaryContactEmail, withdrawn } = req.body as Record<
-        string,
-        string
-      >
+      const {
+        contactEmail,
+        organisationId,
+        referable,
+        secondaryContactEmail,
+        withdrawn,
+        createGeneralOffenceStrand,
+        createSexualOffenceStrand,
+        targetsWomensPrison,
+      } = req.body as Record<string, string>
+
+      if (createGeneralOffenceStrand === 'true' || createSexualOffenceStrand === 'true') {
+        if (createGeneralOffenceStrand === 'true') {
+          const generalOffenceCourses = await this.courseService.getBuildingChoicesVariants(
+            req.user?.username,
+            courseId,
+            { isConvictedOfSexualOffence: 'false', isInAWomensPrison: targetsWomensPrison },
+          )
+          await this.courseService.addCourseOffering(req.user.username, generalOffenceCourses[0].id, {
+            contactEmail,
+            organisationId,
+            referable: referable === 'true',
+            secondaryContactEmail,
+            withdrawn: withdrawn ? withdrawn === 'true' : undefined,
+          })
+        }
+
+        if (createSexualOffenceStrand === 'true') {
+          const sexualOffenceCourses = await this.courseService.getBuildingChoicesVariants(
+            req.user?.username,
+            courseId,
+            { isConvictedOfSexualOffence: 'true', isInAWomensPrison: targetsWomensPrison },
+          )
+          await this.courseService.addCourseOffering(req.user.username, sexualOffenceCourses[0].id, {
+            contactEmail,
+            organisationId,
+            referable: referable === 'true',
+            secondaryContactEmail,
+            withdrawn: withdrawn ? withdrawn === 'true' : undefined,
+          })
+        }
+        return res.redirect(findPaths.buildingChoices.form.show({ courseId }))
+      }
 
       const offering = await this.courseService.addCourseOffering(req.user.username, courseId, {
         contactEmail,
@@ -47,7 +86,7 @@ export default class AddCourseOfferingController {
         withdrawn: withdrawn ? withdrawn === 'true' : undefined,
       })
 
-      res.redirect(findPaths.offerings.show({ courseOfferingId: offering.id }))
+      return res.redirect(findPaths.offerings.show({ courseOfferingId: offering.id }))
     }
   }
 }
