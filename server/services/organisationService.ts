2429b28390480fb7d274ad23f5dde32f8eae850b
pfcairns <phil.feroldi-cairns@digital.justice.gov.uk>
2025-11-04 16:22:07 +0000

    [APG-1475] Fix typo in error message

--- server/services/organisationService.ts
+++ server/services/organisationService.ts
@@ -27,7 +27,7 @@ export default class OrganisationService {
       return await prisonRegisterApiClient.all()
       // eslint-disable-next-line  @typescript-eslint/no-unused-vars
     } catch (error) {
-      throw createError(500, 'Error fetching organisations')
+      throw createError(500, 'Error fetching organisations.')
     }
   }
 
